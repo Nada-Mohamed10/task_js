0e541671af08e6eef39edd83774816c58f968026
Nada Mohamed <144700556+Nada-Mohamed10@users.noreply.github.com>
2024-03-01 11:38:49 +0200

    js

---
+++ script.js
@@ -0,0 +1,122 @@
+// let answer = prompt("Do you fly");
+// if (answer == true) {
+//     let wild = prompt("are you wild");
+//     if (wild == true) {
+//         console.log("Eagle");
+//     } else {
+//         console.log("parrot");
+//     }
+// }
+// else{
+//     let sea = prompt("Do you live under sea");
+//     if (sea == true) {
+//         let wild2 = prompt("Are you wild");
+//         if (wild2 == true) {
+//             console.log("shark");
+//         }
+//     } else {
+//         let wild3 = prompt("are you wild");
+//         if (wild3 ==  true) {
+//             console.log("lion");
+//         }
+//         else {
+//             console.log("cat");
+//         }
+//     }
+// }
+
+
+// let a = 10;
+// let result = a == 10 ? 10 : a >= 10 && a <= 40 ? "10 to 40" : a > 40 ? ">40" : "unknown";
+// console.log(result);
+
+
+// let st = "Elzero web School";
+// let st_length = st.length + st.length;
+
+// if (String(st_length) === "34") {
+//     console.log("Good");
+// }
+// let job = "nada";
+// switch (job) {
+//     case "nada":
+//         console.log("nada");
+//         break;
+//     case "mosad":
+//         console.log("mosad");
+//         break;
+//     default:
+//         console.log("un known")
+// }
+
+// let job = "manager";
+// let salary = 0;
+// switch (job) {
+//     case "manager":
+//         console.log("salary= 8000")
+//         break;
+//     case "IT":
+//     case "support":
+//         console.log("salary=6000");
+//         break;
+//     case "Devolper":
+//     case "Desiginer":
+//         console.log("salary=7000");
+//         break;
+//     default:
+//         console.log('salary=4000');
+// }
+
+
+
+// for (let i = 0; i <= 100; i++){
+//     if (i % 2 == 0) {
+//         console.log(i);
+//     }
+// }
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 3 == 0) {
+//         console.log("fizz");
+//     }
+//     else if (i % 5 == 0) {
+//         console.log("buzz");
+//     }
+//     else if (i % 3 == 0 && i % 5 == 0) {
+//         console.log("fizz buzz");
+//     }
+//     else if (i % 3 != 0 && i%5 != 0) {
+//         console.log("number itself")
+
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 3 == 0 && i % 5 == 0) {
+//         console.log("fizz buzz");
+//     }
+//     else if (i % 5 == 0) {
+//         console.log("buzz");
+//     }
+//     else if (i % 3 == 0) {
+//         console.log("fizz ");
+//     }
+//     else {
+//         console.log(i);
+//     }
+// }
+
+
+
+
+// let my = ["ahmed", "nada", "mosad", "naddosh", "mohamed", "ameer"];
+// let myNew = my.slice(2);
+// console.log(myNew.slice(-4, -2))
+// let arr = [3, 2, 1, 2];
+// console.log(arr.sort());
+
+
+
+
+
+
+
+
